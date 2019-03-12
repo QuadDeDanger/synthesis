e3040a9256c7b31860f28a2123224cdb5ab8fddc
g16m3797 <g16m3797@campus.ru.ac.za.here>
2019-03-12 11:45:25 +0200

    Prac2 F#

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -59,9 +59,6 @@ let digits a =
 
 let minmax (a, b, c, d) = min a b |> min c |> min d, max a b |> max c |> max d
   
-  
-
-
 
 let isLeap a =
  match a<1582 with
@@ -73,14 +70,54 @@ let isLeap a =
    
 
 
-let month _ =
-    failwith "Not implemented"
+let month m =
+ match m with
+  |1-> ("January", 31)
+  |2-> ("February", 28)
+  |3-> ("March", 31)
+  |4-> ("April", 30)
+  |5-> ("May", 31)
+  |6-> ("June", 30)
+  |7-> ("July", 31)
+  |8-> ("August", 31)
+  |9-> ("September", 30)
+  |10-> ("October", 31)
+  |11-> ("November", 30)
+  |12-> ("December", 31)
+  |_-> failwith "an integer less than 1 or greater than 12 is supplied"
+  
 
-let toBinary _ =
-    failwith "Not implemented"
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let toBinary b =
+ let rec toBinaryString i acc=
+  match i=0 && acc<>"" with
+   |true-> acc
+   |false->
+       match i%2<>0 with
+       |true-> toBinaryString (i/2) ("1" + acc)
+       |false-> toBinaryString(i/2) ("0" + acc)
+ match b<0 with
+  |true -> failwith "Not implemented"
+  |_-> toBinaryString b ""
+
+
+let bizFuzz n =
+ let rec count v x y z=
+  match v>n || n<0 with 
+  |true-> (x,y,z)
+  |false -> match (v%3=0 && v%5=0)  with 
+             |true-> count (1+v) (1 + x) (1+y) (1+z)
+             |false-> match v%3=0 with
+                       |true-> count (1+v) (1+x) (y) (z)
+                       |false-> match v%5=0 with 
+                                 |true-> count (1+v) (x) (1+y) (z)
+                                 |false->count (1+v) x y z        
+ match n<0 with
+  |true-> (0,0,0)
+  |false-> count 1 0 0 0
+
+
+ 
 
 let monthDay _ _ =
     failwith "Not implemented"
